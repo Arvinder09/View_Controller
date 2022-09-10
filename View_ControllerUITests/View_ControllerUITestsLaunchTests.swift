58a3213cbae5cc8c29d53156f9297fd9597fe74b
“Arvinder” <Arvinder.Bhurji@ril.com>
2022-09-10 12:42:11 +0530

    UI tests

---
+++ View_ControllerUITests/View_ControllerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  View_ControllerUITestsLaunchTests.swift
+//  View_ControllerUITests
+//
+//  Created by Arvinder Bhurji on 02/09/22.
+//
+
+import XCTest
+
+class View_ControllerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
